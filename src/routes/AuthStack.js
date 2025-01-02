e55ad68cd9cfeacac7d90c53da4bd4dfbcb7dd5f
Zakwan Malik <zakwanmalik85@gmail.com>
2025-01-02 16:27:19 +0530

    demo

--- src/routes/AuthStack.js
+++ src/routes/AuthStack.js
@@ -1,17 +1,18 @@
 // AuthStack.js
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Login, Otp, Splash} from '../screens';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login, Otp, Splash } from '../screens';
 
 const AuthStack = createNativeStackNavigator();
 
 export default function AuthStackScreen() {
+  console.log("first")
   return (
-    <AuthStack.Navigator screenOptions={{headerShown: false}}>
+    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
       <AuthStack.Screen
         name="Splash"
         component={Splash}
-        options={{title: 'Welcome'}}
+        options={{ title: 'Welcome' }}
       />
       <AuthStack.Screen name="Login" component={Login} />
       <AuthStack.Screen name="Otp" component={Otp} />
